19b1681bb1649e77f937b546cc77121152b134c5
LinkusGamer <theskyrimpls@gmail.com>
2026-05-16 11:57:47 -0500

    Add RootLayout component and metadata for app

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'The Festuscrate Demonlist',
+  description: 'Geometry Dash leaderboard website',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
